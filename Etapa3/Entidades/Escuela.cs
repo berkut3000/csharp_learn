d7121b2416d13cd0948d5a2bc67e5a59a7665b41
Antonio Mota <berkut3000@hotmail.com>
2022-08-10 15:47:34 -0500

    Add Etapa3

---
+++ Etapa3/Entidades/Escuela.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+namespace CoreEscuela.Entidades
+{
+    class Escuela
+    {
+        string nombre;
+        public string Nombre
+        {
+            get{ return "Copia:" + nombre; }
+            set{ nombre = value.ToUpper(); }
+        }
+        
+        public int añoCreacion{ get; set;}
+
+        public string Pais { get; set; }
+        public string Ciudad { get; set; }
+
+        public TiposEscuela tiposEscuela { get; set; }
+
+        public List<Curso> Cursos { get; set; }
+        //public Curso[] Cursos { get; set; }
+
+        /*         public Escuela(string nombre, int año)
+                {
+                    this.nombre = nombre;
+                    this.añoCreacion = año;
+                } */
+        public Escuela(string nombre, int año) => (Nombre, añoCreacion) = (nombre, año);
+
+        public override string ToString()
+        {
+            return $"Nombre:  \"{Nombre}\" \r\nTipo: \"{tiposEscuela}\" \r\nPais: \"{Pais}\" {System.Environment.NewLine}Ciudad: \"{Ciudad}\"";
+        }
+
+        
+    }
+}
