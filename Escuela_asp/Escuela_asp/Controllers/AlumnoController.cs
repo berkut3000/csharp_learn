036f5b01bfedcfb943e81e8034d895469e9f5888
Antonio Aguilar <berkut3000@hotmail.com>
2022-12-23 19:52:41 -0600

    Add MultiAlumno Functionality

---
+++ Escuela_asp/Escuela_asp/Controllers/AlumnoController.cs
@@ -0,0 +1,45 @@
+﻿using Escuela_asp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Escuela_asp.Controllers
+{
+    public class AlumnoController : Controller
+    {
+        public IActionResult Index()
+        {
+            var alumno = new Alumno
+            {
+                Nombre = "Pepe Perez",
+                UniqueId = Guid.NewGuid().ToString()
+            };
+
+            return View(alumno);
+        }
+        public IActionResult Multialumno()
+        {
+            var listaAlumnos = GenerarAlumnosAlAzar();
+
+
+            ViewBag.CosaDinamica = "La Monja";
+            ViewBag.Fecha = DateTime.UtcNow;
+
+            return View("Multialumno",listaAlumnos);
+        }
+
+        private List<Alumno> GenerarAlumnosAlAzar()
+        {
+            string[] nombre1 = { "Alba", "Felipa", "Eusebio", "Farid", "Donald", "Alvaro", "Nicolás" };
+            string[] apellido1 = { "Ruiz", "Sarmiento", "Uribe", "Maduro", "Trump", "Toledo", "Herrera" };
+            string[] nombre2 = { "Freddy", "Anabel", "Rick", "Murty", "Silvana", "Diomedes", "Nicomedes", "Teodoro" };
+
+            var listaAlumnos = from n1 in nombre1
+                               from n2 in nombre2
+                               from a1 in apellido1
+                               select new Alumno { Nombre = $"{n1} {n2} {a1}", UniqueId = Guid.NewGuid().ToString() };
+
+            return listaAlumnos.OrderBy((al) => al.UniqueId).ToList();
+        }
+
+    }
+}
