8bf0a35480030ac4faab7b023647a94c7bcb3452
Antonio Aguilar <berkut3000@hotmail.com>
2022-12-23 18:15:16 -0600

    Add partial views, usage of tag helper partial use of foreach to list Created AsignaturaSimpe template

--- Escuela_asp/Escuela_asp/Controllers/AsignaturaController.cs
+++ Escuela_asp/Escuela_asp/Controllers/AsignaturaController.cs
@@ -7,15 +7,44 @@ namespace Escuela_asp.Controllers
     {
         public IActionResult Index()
         {
-            var asignatura = new Asignatura {
-                UniqueId = Guid.NewGuid().ToString(),
-                Nombre = "Matemáticas"
+            var asignatura = new Asignatura
+            {
+                Nombre = "Programacion",
+                UniqueId = Guid.NewGuid().ToString()
             };
+
+            return View(asignatura);
+        }
+        public IActionResult Multiasignatura()
+        {
+            var listaAsignaturas = new List<Asignatura>() {
+            new Asignatura {
+                    Nombre = "Matemáticas",
+                    UniqueId = Guid.NewGuid ().ToString ()
+                    },
+                    new Asignatura {
+                    Nombre = "Educación Física",
+                    UniqueId = Guid.NewGuid ().ToString ()
+                    },
+                    new Asignatura {
+                    Nombre = "Castellano",
+                    UniqueId = Guid.NewGuid ().ToString ()
+                    },
+                    new Asignatura {
+                    Nombre = "Ciencias Naturales",
+                    UniqueId = Guid.NewGuid ().ToString ()
+                    },
+                    new Asignatura {
+                    Nombre = "Programacion",
+                    UniqueId = Guid.NewGuid ().ToString ()
+                    }
+            };
+
 
             ViewBag.CosaDinamica = "La Monja";
             ViewBag.Fecha = DateTime.UtcNow;
 
-            return View(asignatura);
+            return View("Multiasignatura",listaAsignaturas);
         }
 
     }
