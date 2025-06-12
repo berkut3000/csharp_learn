0a0eb099feebd902aed8658bb430bc6fc7118565
Antonio Mota <berkut3000@hotmail.com>
2025-06-12 10:23:33 -0600

    Add HDLeon Creacion de Objetos

---
+++ HDLeon_001_creacion_de_objetos/HDLeon_001_creacion_de_objetos/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Creacion de Objetos");
+Console.WriteLine();
+
+var sale = new Sale(15);
+var message = sale.GetInfo();
+Console.WriteLine(message);
+
+
+class Sale
+{
+    public decimal Total { get; set; }
+    private decimal _some;
+
+    public Sale(decimal total)
+    {
+        Total = total;
+    }
+
+    public string GetInfo()
+    {
+        return "El total es " + Total;
+    }
+}
